d1854d07016fbd22ff032409f71c6f00d9a30c20
user <user@DESKTOP-Q7SRQOE>
2023-11-05 20:12:23 +0200

    Add project files.

---
+++ BalancedBracketsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class BalancedBracketsTests
+{
+    [Test]
+    public void Test_IsBalanced_EmptyInput_ShouldReturnTrue()
+    {
+        string[] input = { };
+        bool result = BalancedBrackets.IsBalanced(input);
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_BalancedBrackets_ShouldReturnTrue()
+    {
+        string[] input = { "(", ")", "(", ")", "(", ")" };
+        bool result = BalancedBrackets.IsBalanced(input);
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_UnbalancedBrackets_ShouldReturnFalse()
+    {
+        string[] input = { ")", "(", ")" };
+        bool result = BalancedBrackets.IsBalanced(input);
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_MoreClosingBrackets_ShouldReturnFalse()
+    {
+        string[] input = { "(", ")", "(", ")", ")" };
+        bool result = BalancedBrackets.IsBalanced(input);
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Test_IsBalanced_NoClosingBrackets_ShouldReturnFalse()
+    {
+        string[] input = { ")", "(" };
+        bool result = BalancedBrackets.IsBalanced(input);
+        Assert.IsFalse(result);
+    }
+  
+
+}
